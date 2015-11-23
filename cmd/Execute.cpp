36877e81fd1fbc9ccd61d914b672f87b15f7ca40
ShadyF <shadster09@gmail.com>
2015-11-23 19:43:58 +0200

    Added Execute.cpp

---
+++ cmd/Execute.cpp
@@ -0,0 +1,15 @@
+#include "Execute.h"
+#include <string>
+#include <vector>
+
+Execute::Execute()
+{}
+
+Execute::~Execute()
+{}
+
+std::vector<int> Execute::run(std::vector<int> Decode_buffer, std::vector<int> F_DM, 
+						std::vector<int> F_WB)
+{
+	int alu_first_op, alu_second_op;
+}
