b50a88b66698e6abe8517801fd65ef8bb6a14f57
ShadyF <shadster09@gmail.com>
2015-11-25 19:56:08 +0200

    Initialised DataMemory vector

--- cmd/CPU.cpp
+++ cmd/CPU.cpp
@@ -6,21 +6,22 @@
 
 CPU::CPU(std::string filename)
 {
-  Parser Parse;
-  InsMemory = Parse.run(filename);
-  FetchStage.init(InsMemory);
+	Parser Parse;
+	InsMemory = Parse.run(filename);
+	FetchStage.init(InsMemory);
+	DataMemory.resize(32);
 	clock = 0;
 	Regs.resize(16);
 }
 
 CPU::~CPU()
 {
-	
+
 }
 
 void CPU::run_one_cycle()
 {
-	
+
 }
 
 void CPU::rst()
@@ -28,5 +29,5 @@ void CPU::rst()
 	//set fetch PC to 0
 	//set regs to 0
 	//set clock to 0
-	
+
 }
