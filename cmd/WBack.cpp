ad911edf20f849a2b349be6f7250a950874e2bdc
ShadyF <shadster09@gmail.com>
2015-11-25 19:54:42 +0200

    Fixed vector size + added pointer (DONE!)

--- cmd/WBack.cpp
+++ cmd/WBack.cpp
@@ -3,12 +3,14 @@
 #include "Memory.h"
 using namespace std;
 
-WBack::WBack()
-{};
+WBack::WBack(vector<int>* Regs) : RegFile(Regs)
+{
+	WB_Forwarding_Unit.resize(4);
+};
 WBack::~WBack()
 {};
 
-int Mux_Result;	//ay khara (Register File from Amr)
+//int Mux_Result;	//ay khara (Register File from Amr)
 
 void WBack::WB_run(vector<int> Memory_Buffer)
 {
@@ -19,14 +21,17 @@ void WBack::WB_run(vector<int> Memory_Buffer)
 		ALU_Result = Memory_Buffer[3],
 		RT_or_RD = Memory_Buffer[4];
 
-	if (RegWrite && !MemToReg)
+	if (!MemToReg) //if MemToReg = 0 ALU_Result
 		Mux_Result = ALU_Result;
-	else if (RegWrite && MemToReg)
+	else if (MemToReg)  //if MemToReg = 1 Mem_Data
 		Mux_Result = Mem_Data;
+
+	if (RegWrite)
+		RegFile->at(RT_or_RD) = Mux_Result;
 
 };
 
-vector<int> WBack::Send_FU_WB_To_Execute(vector<int> Memory_Buffer) 
+vector<int> WBack::Send_FU_WB_To_Execute(vector<int> Memory_Buffer)
 {
 	// Extracting Memory Buffer
 	int RegWrite = Memory_Buffer[0],
@@ -35,13 +40,12 @@ vector<int> WBack::Send_FU_WB_To_Execute(vector<int> Memory_Buffer)
 
 
 	//WB_Forwarding_Unit [Size=4]
-	WB_Forwarding_Unit.push_back(RegWrite);		//0
-	WB_Forwarding_Unit.push_back(MemToReg);		//1
-	WB_Forwarding_Unit.push_back(Mux_Result);	//2
-	WB_Forwarding_Unit.push_back(RT_or_RD);		//3
+	WB_Forwarding_Unit[0] = RegWrite;		//0
+	WB_Forwarding_Unit[1] = MemToReg;		//1
+	WB_Forwarding_Unit[2] = Mux_Result;	//2
+	WB_Forwarding_Unit[3] = RT_or_RD;		//3
 
-	//WB_Forwarding_Unit is the full buffer of WB + MuxResult + RT/RD Address
+												//WB_Forwarding_Unit is the full buffer of WB + MuxResult + RT/RD Address
 	return WB_Forwarding_Unit;
 };
-
 
