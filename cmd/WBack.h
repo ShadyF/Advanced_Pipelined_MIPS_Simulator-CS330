e4e768d1b136d3af56e03d47da663b75890fc93c
ShadyF <shadster09@gmail.com>
2015-11-25 19:55:08 +0200

    Added pointer to RegFile (DONE!)

--- cmd/WBack.h
+++ cmd/WBack.h
@@ -1,21 +1,22 @@
 #ifndef WBACK_H
 #define WBACK_H
 #include <vector>
-#include "Memory.h"
 using namespace std;
 
 //Write Back Stage
 
-class WBack: public Memory
+class WBack
 {
 public:
-	WBack();
+	WBack(vector<int>* RegFile);
 	void WB_run(vector <int>);			//Run (Doesn't return a buffer) (Final Stage)
 	vector <int> Send_FU_WB_To_Execute(vector <int>);	//returns Forwarding Unit
 	~WBack();
 
 	vector <int> WB_Forwarding_Unit;		//WB + Mux Result + RT/RD Address
-	vector <int> Memory_Buffer;			//Taking Memory_Buffer.
+	//vector <int> Memory_Buffer;			//Taking Memory_Buffer.
+	vector<int>* RegFile;
+	int Mux_Result;
 };
 
 #endif
